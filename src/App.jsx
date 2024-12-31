974254e2c88bf85cfca5ee5fbcedc079decb3b5c
yy soldier <bladerunner13101337@gmail.com>
2024-12-31 22:33:29 +0300

    added posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
-import Header from './components/Header'
+import Header from './components/Header';
+import Post from './components/Post';
 function App() {
   return (
     <>
       <Header/>
+      <div className="posts_view">
+        <Post title='Nosferatu' description='This is such a good movie!'/> 
+        <Post title='Dumb and dumber' description='Dogshit!'/> 
+      </div>
     </>
   )
 }
